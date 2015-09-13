fb0b883db886332837fae4848d60d2c904aa785a
robsonbittencourt <robson.luizv@gmail.com>
2015-09-13 16:59:28 -0300

    Refactored wait mechanism to use injectors

--- src/main/java/com/mokona/core/MokonaElement.java
+++ src/main/java/com/mokona/core/MokonaElement.java
@@ -1,7 +1,5 @@
 package com.mokona.core;
 
-import static com.mokona.wait.WaitTool.waitForElement;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -9,11 +7,15 @@ import org.openqa.selenium.WebElement;
 
 import com.google.inject.Inject;
 import com.mokona.exception.MokonaException;
+import com.mokona.wait.WaitTool;
 
 public abstract class MokonaElement {
     
     @Inject
     private WebDriver webDriver;
+    
+    @Inject
+    private WaitTool waitTool;
 
     private WebElement webElement;
 
@@ -34,7 +36,7 @@ public abstract class MokonaElement {
     private WebElement findElement() {
         try {
             webElement = webDriver.findElement(By.cssSelector(selector));
-            waitForElement(webDriver, By.cssSelector(selector), 30);
+            waitTool.waitForElement(By.cssSelector(selector), 30);
         } catch (NoSuchElementException e) {
             throw new MokonaException("Element not found on the page.");
         }
--- src/main/java/com/mokona/wait/WaitTool.java
+++ src/main/java/com/mokona/wait/WaitTool.java
@@ -8,39 +8,39 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.google.inject.Inject;
+
 /**
  * 
- * Adapted from https://github.com/ChonC/wtbox/blob/master/src/wtbox/util/WaitTool.java#L194
+ * Adapted from
+ * https://github.com/ChonC/wtbox/blob/master/src/wtbox/util/WaitTool.java#L194
  * 
- * @author Chon Chung, Mark Collin, Andre, Tarun Kumar 
+ * @author Chon Chung, Mark Collin, Andre, Tarun Kumar
  * 
- * Copyright [2012] [Chon Chung]
+ *         Copyright [2012] [Chon Chung]
  * 
- * Licensed under the Apache Open Source License, Version 2.0  
- * http://www.apache.org/licenses/LICENSE-2.0
+ *         Licensed under the Apache Open Source License, Version 2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  */
 
 public class WaitTool {
 
-    public static final int DEFAULT_WAIT_4_ELEMENT = 15; 
-    public static final int DEFAULT_WAIT_4_PAGE = 30; 
+    public static final int DEFAULT_WAIT_4_ELEMENT = 15;
+    public static final int DEFAULT_WAIT_4_PAGE = 30;
 
-    public static WebElement waitForElement(WebDriver driver, final By by, int timeOutInSeconds) {
-        WebElement element; 
-        try{	
-            driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+    @Inject
+    private WebDriver webDriver;
 
-            WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds); 
-            element = wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    public WebElement waitForElement(final By by, int timeOutInSeconds) {
+        webDriver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
 
-            driver.manage().timeouts().implicitlyWait(DEFAULT_WAIT_4_PAGE, TimeUnit.SECONDS); 
-            return element; 	
-        } catch (Exception e) {
-            e.printStackTrace();
-        } 
-        return null; 
+        WebDriverWait wait = new WebDriverWait(webDriver, timeOutInSeconds);
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+
+        webDriver.manage().timeouts().implicitlyWait(DEFAULT_WAIT_4_PAGE, TimeUnit.SECONDS);
+ 
+        return element;
     }
-
 
 }
